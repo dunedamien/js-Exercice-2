7a035edc7985551edd47771a7a9006173a36602e
Dune Damien <d.dune.simplon@gmail.com>
2018-08-08 22:42:44 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+alert("Exercice 1!");
+var nom = prompt("Quel est ton nom?", "Entrez ici votre prénom")
+alert("Bonjour " + nom + ", comment va tu?")
+console.log("Bonjour ",nom,", Comment va tu?");
+var nom = document.getElementById("result");
+Réponse.innerHTML = nom ;
---
+++ README.md
@@ -0,0 +1,1 @@
+# js-Exercice-2
